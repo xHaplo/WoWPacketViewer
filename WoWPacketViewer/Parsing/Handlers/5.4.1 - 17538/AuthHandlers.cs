491d433b17d3322c7833e5258baf7b2be341c1cb
twostars <i.am.twostars@gmail.com>
2013-11-22 21:42:54 +1100

    Added handler for SMSG_AUTH_CHALLENGE.

---
+++ WoWPacketViewer/Parsing/Handlers/5.4.1 - 17538/AuthHandlers.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using CustomExtensions;
+using WoWPacketViewer.Enums;
+using WoWPacketViewer.Misc;
+
+namespace WoWPacketViewer.Parsing.Handlers.V541_17538
+{
+    using Guid = WoWPacketViewer.Misc.Guid;
+
+    [ClientBuild(17538)]
+    public static class AuthHandlers
+    {
+        [Parser(Opcode.SMSG_AUTH_CHALLENGE, 0x0C5D)]
+        public static void HandleAuthChallenge(Packet packet)
+        {
+            byte response = packet.ReadByte("Response");
+            var key = new uint[8];
+            uint seed;
+
+            for (int i = 0; i < key.Length; i++)
+                key[i] = packet.ReadUInt32("Key {0}", i + 1);
+
+            seed = packet.ReadUInt32("Seed");
+        }
+    }
+}
