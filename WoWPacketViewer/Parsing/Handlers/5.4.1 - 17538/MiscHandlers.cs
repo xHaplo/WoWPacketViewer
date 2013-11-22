3b7fb8d3636babe188b3dbb6b6b43089e884be5e
twostars <i.am.twostars@gmail.com>
2013-11-22 23:05:32 +1100

    Implemented handler for SMSG_CLIENTCACHE_VERSION.

---
+++ WoWPacketViewer/Parsing/Handlers/5.4.1 - 17538/MiscHandlers.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using CustomExtensions;
+using WoWPacketViewer.Enums;
+using WoWPacketViewer.Misc;
+
+namespace WoWPacketViewer.Parsing.Handlers.V541_17538
+{
+    using Guid = WoWPacketViewer.Misc.Guid;
+
+    [ClientBuild(17538)]
+    public static class MiscHandlers
+    {
+        [Parser(Opcode.SMSG_CLIENTCACHE_VERSION, 0x1037)]
+        public static void HandleAuthResponse(Packet packet)
+        {
+            uint cacheVersion = packet.ReadUInt32("CacheVersion");
+        }
+    }
+}
