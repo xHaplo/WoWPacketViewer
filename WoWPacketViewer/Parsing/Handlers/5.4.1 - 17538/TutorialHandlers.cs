b0caf04de8473052eb27cec94c3fc202cf6e3399
twostars <i.am.twostars@gmail.com>
2013-11-22 23:09:47 +1100

    Implemented handler for SMSG_TUTORIAL_FLAGS.

---
+++ WoWPacketViewer/Parsing/Handlers/5.4.1 - 17538/TutorialHandlers.cs
@@ -0,0 +1,17 @@
+﻿using WoWPacketViewer.Enums;
+using WoWPacketViewer.Misc;
+
+namespace WoWPacketViewer.Parsing.Handlers.V541_17538
+{
+    [ClientBuild(17538)]
+    public static class TutorialHandlers
+    {
+        [Parser(Opcode.SMSG_TUTORIAL_FLAGS, 0x0D1B)]
+        public static void HandleTutorialFlags(Packet packet)
+        {
+            var tutorialFlags = new uint[8];
+            for (var i = 0; i < 8; i++)
+                tutorialFlags[i] = packet.ReadUInt32("Flag {0}", i);
+        }
+    }
+}
